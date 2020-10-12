6efc44a798395c0918d71930d27b69d8b10d1962
yazid <larroum.my@gmail>
2020-10-12 09:20:20 +0100

    final reactCheck

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container d-flex align-items-center justify-content-center">
+        <form className="login-form text-center">
+            <h1 className="mn-5 font-weight-light text-uppercase">Login</h1>
+            <div className="form-group">
+                <input type="text" className="form-control" placeholder="username"/>
+            </div>
+            <div className="form-group">
+               <input type="password" className="form-control" placeholder="Password"/>
+            </div>
+            <div className="forgot-link d-flex align-items-center justify-content-between"> 
+                 <div className= "form-check">
+                   <input type="checkbox" className="form-check-input" id="remember"/>
+                   <label for ="remember">Remember Password</label>
+                 </div>
+                 <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit" className="btn mt-5 btn-primary btn-block">Login</button>
+            <p className="mt-3 font-weight-normal"> Don't have an account <a href ="#">Register Now</a></p>
+        </form>
     </div>
   );
 }
